d3aac934f3b1f065bffd00673aa9869d68b872fb
BrentonBasinger <bbasinger@mail.usf.edu>
2017-02-09 01:39:44 +0000

    Here is my stuff

--- brenton2/Test.py
+++ brenton2/Test.py
@@ -1,1 +1,14 @@
-something
+Plusline = ("+----+----+")
+Line = ("|    |    |")
+
+
+print(Plusline)
+print(Line)
+print(Line)
+print(Line)
+print(Plusline)
+print(Line)
+print(Line)
+print(Line)
+print(Plusline)
+
